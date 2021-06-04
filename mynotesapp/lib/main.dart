cc097160022684993d3d9bd24cd1477f02e68ba0
mufassalhussain <79256743+mufassalhussain@users.noreply.github.com>
2021-06-04 20:07:24 +0500

    update data
    update data

--- mynotesapp/lib/main.dart
+++ mynotesapp/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:test_app_firebase/editnote.dart';
+import 'package:mynotesapp/editnote.dart';
 import 'package:flutter/material.dart';
-import 'package:test_app_firebase/addnote.dart';
+import 'package:mynotesapp/addnote.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
