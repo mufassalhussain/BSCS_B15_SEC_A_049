f9a0ac582d4da8a0073a6946b302df9f548af374
mufassalhussain <hmufassal@gmail.com>
2021-07-02 14:44:39 +0500

    app icon and splash animation
    app icon and splash animation

--- crud_php/lib/splash.dart
+++ crud_php/lib/splash.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 //import 'package:crud_php/main.dart';
 import 'package:flutter/material.dart';
 import 'package:crud_php/home.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
 
 class splashscreen extends StatefulWidget {
   @override
@@ -23,50 +24,25 @@ class _splashscreenState extends State<splashscreen> {
   // and made the text to align at center
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 70.0,
-        centerTitle: true,
-        title: Text(
-          'Welcome to CRUD PHP MYSQL APP',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
-        ),
-
-        // leading: IconButton(
-        //  icon: Icon(Icons.menu),
-        //onPressed: () {
-        // NavDrawer();
-        //},
-        // ),
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight,
-              colors: [Colors.red, Colors.blue],
-            ),
-          ),
-        ),
+    return SplashScreenView(
+      navigateRoute: mainhome(),
+      duration: 10000,
+      imageSize: 300,
+      imageSrc: "assets/Mufassalprofile.png",
+      text: "Mufassal Hussain \nFA17-BCS-049",
+      textType: TextType.ColorizeAnimationText,
+      textStyle: TextStyle(
+        fontSize: 30.0,
+        fontWeight: FontWeight.bold,
       ),
+      colors: [
+        Colors.amberAccent,
+        Colors.tealAccent,
+        Colors.deepOrange,
+        Colors.red,
+        Colors.green,
+      ],
       backgroundColor: Colors.blue,
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Expanded(
-            child: Image.asset(
-              'assets/annimation.gif',
-            ),
-          ),
-          Expanded(
-            child: Text(
-              'Name : Mufassal Hussain\n Reg#: FA17-BCS-049',
-              style: TextStyle(fontSize: 20.0, color: Colors.red),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ],
-      )),
     );
   }
 }
